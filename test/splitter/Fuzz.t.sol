65c1636cf515c4d9712ca80eff4640f09d688d58
Rafael Nicolau <0xrafaelnicolau@gmail.com>
2023-09-11 01:58:10 +0100

    add: fuzz tests for whitelist funcs

--- test/splitter/Fuzz.t.sol
+++ test/splitter/Fuzz.t.sol
@@ -5,6 +5,26 @@ import {Setup} from "../Setup.t.sol";
 import {Splitter} from "../../src/Splitter.sol";
 
 contract Fuzz is Setup {
+    function testFuzzAddToken(address token) public {
+        vm.assume(token != address(0));
+
+        vm.prank(deployer);
+        splitter.addToken(token);
+
+        assertEq(splitter.tokenWhitelist(token), true);
+    }
+
+    function testFuzzRemoveToken(address token) public {
+        vm.assume(token != address(0));
+
+        testFuzzAddToken(token);
+
+        vm.prank(deployer);
+        splitter.removeToken(token);
+
+        assertEq(splitter.tokenWhitelist(token), false);
+    }
+
     function testFuzzAddProposer(address proposer) public {
         vm.assume(proposer != address(0));
 
@@ -15,9 +35,9 @@ contract Fuzz is Setup {
     }
 
     function testFuzzRemoveProposer(address proposer) public {
-        testFuzzAddProposer(proposer);
-
         vm.assume(proposer != address(0));
+
+        testFuzzAddProposer(proposer);
 
         vm.prank(deployer);
         splitter.removeProposer(proposer);
